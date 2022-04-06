96cf74f27771aa54b42fc4faad986c15755abd89
Justin Li <justinlit23@gmail.com>
2022-04-06 15:15:35 -0700

    commit tree fda319f4eb1e48c60594c81c9f85334d38a9665f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const writeCards = (arr, event) => {
+    const thankYouArray = [...arr];
+    for (let i=0; i < thankYouArray.length; i++) {
+        thankYouArray[i]=`Thank you, ${arr[i]}, for the wonderful ${event} gift!`;
+    }
+    return thankYouArray;
+}
+
+const countDown = (int) => {
+    for (int=int; int >= 0; int--) {
+        console.log(int)
+    }
+}
